aca2aaf52a8152daacf6113fd1795ffecde222b8
Hadro0n <v.w.yakunin@gmail.com>
2024-12-24 18:54:41 +0300

    Create PersonalQuiz - pt. 1

---
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Владислав Якунин on 24.12.2024.
+//
+
+import UIKit
+
+final class QuestionViewController: UIViewController {
+
+    private let question = Question.getQuestions()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ResultViewController.swift
 //  PersonalQuiz
 //
 //  Created by Владислав Якунин on 24.12.2024.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
