56e73e813f0321ed46e0be6a59f91aa93f6781b5
Hadro0n <v.w.yakunin@gmail.com>
2025-01-10 14:07:07 +0300

    Personal Quiz (last)

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,8 +9,47 @@ import UIKit
 
 final class ResultViewController: UIViewController {
 
+    @IBOutlet var animalTypeLabel: UILabel!
+    @IBOutlet var definitionLabel: UILabel!
+    
+    var answers: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        updateResult()
+    }
+    
+    @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    deinit {
+        print("\(type(of: self)) has been deallocted")
+    }
+}
+
+extension ResultViewController {
+    private func updateResult() {
+        var choiceOfAnimals: [Animal: Int] = [:]
+        let animals = answers.map { $0.animal }
+        
+        for animal in animals {
+            if let animalTypeCount = choiceOfAnimals[animal] {
+                choiceOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                choiceOfAnimals[animal] = 1
+            }
+            
+            let sortedChoiceOfAnimals = choiceOfAnimals.sorted { $0.value > $1.value }
+            guard let mostChoiceAnimal = sortedChoiceOfAnimals.first?.key else { return }
+            updateUI(with: mostChoiceAnimal)
+        }
+    }
+    
+    private func updateUI(with animal: Animal) {
+        animalTypeLabel.text = "Вы - \(animal.rawValue)!"
+        definitionLabel.text = animal.definition
     }
     
 }
